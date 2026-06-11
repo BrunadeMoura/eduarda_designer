1d5fad49d85a00068db7ef5421069275d682b301
Bruna Moura <brunninhademoura@gmail.com>
2026-06-11 14:56:04 -0300

    Mapa Leaflet estilizado.

--- map.js
+++ map.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+const lat = -23.152896;
+const lng = -47.006805;
+
+var map = L.map('map', {
+    minZoom: 14,
+    maxZoom: 17,
+    dragging: false,
+}).setView([lat, lng], 15);
+var marker = L.marker([lat, lng]).addTo(map);
+
+marker.bindPopup(`<b>Studio Eduarda Almeida</b></br><a href="https://www.google.com/maps?q=${lat},${lng}" target="_blank" style="display:flex;">Abrir no Google Maps <img src="assets/icons/Arrow up-right.svg" style="width: 1rem; height: auto; filter: invert(1) brightness(2) saturate(0);"/></a>`).openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
